a02433ffa7087155c14528bd6fd337801d58b01e
Nick Franczak <46872531+nfranczak@users.noreply.github.com>
2021-09-16 03:22:23 -0400

    Update MyPeripheralManager.swift

--- point clouds/MyPeripheralManager.swift
+++ point clouds/MyPeripheralManager.swift
@@ -4,39 +4,41 @@
 //
 //  Created by Nick Franczak on 9/14/21.
 //
-
 import Foundation
 import CoreBluetooth
 import UIKit
 
 class MyPeripheralManager: NSObject, CBPeripheralManagerDelegate{
     static let shared = MyPeripheralManager()
+    
+
     var peripheralManager: CBPeripheralManager = CBPeripheralManager()
-    
-    
+
     var serviceUUID: CBUUID = CBUUID()
     var characteristicUUID: CBUUID = CBUUID()
     let advertisementDataLocalNameKey : String = "max8Chars"
-    
+
     func start() {
+        print("1")
         let deviceUUID: String = UIDevice.current.identifierForVendor!.uuidString
         serviceUUID = CBUUID(string: deviceUUID)
         characteristicUUID = CBUUID(string: deviceUUID)
         if !peripheralManager.isAdvertising{
+            print("2")
+            print(CBAdvertisementDataIsConnectable)
             peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
         }
 
     }
-    
+    // before 3 is printed we get this message: [CoreBluetooth] XPC connection invalid
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
         switch self.peripheralManager.state {
         case .poweredOn:
-            print("peripheral powered on")
-            
+            print("3")
             let _: CBMutableService = CBMutableService(type: serviceUUID, primary: true)
-            
+
             let _: CBMutableCharacteristic = CBMutableCharacteristic(type: characteristicUUID, properties: [.write, .read], value: nil, permissions: [.writeable, .readable])
-            
+
         case .poweredOff:
             print("peripheral powered off")
         case .resetting:
@@ -51,17 +53,18 @@ class MyPeripheralManager: NSObject, CBPeripheralManagerDelegate{
             print("peripheral unknown")
         }
     }
-    
-    func peripheralManager(_ peripheral: CBPeripheralManager, willRestoreState dict: [String : Any]) {
-        
+
+    func peripheralManager(_ peripheral: CBPeripheralManager) {
+        print("4")
         peripheral.delegate = self
-        
+
         if !self.peripheralManager.isAdvertising {
             self.peripheralManager.startAdvertising([CBAdvertisementDataServiceUUIDsKey: [serviceUUID], CBAdvertisementDataLocalNameKey: advertisementDataLocalNameKey])
         }
     }
-    
+
     func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveRead request: CBATTRequest) {
+        print("5")
         var arr: [UInt8] = [0,0,0,0,0] // example data
         let value: Data = Data(bytes: &arr, count: arr.count)
         request.value = value
@@ -69,6 +72,7 @@ class MyPeripheralManager: NSObject, CBPeripheralManagerDelegate{
     }
     
     func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite requests: [CBATTRequest]) {
+        print("6")
         if let request = requests.first {
             if let value = request.value {
                 let valueBytes: [UInt8] = [UInt8](value)
