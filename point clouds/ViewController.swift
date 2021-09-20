136a810a15c297ac861cb7aba4af317dcc399ea6
Nick Franczak <46872531+nfranczak@users.noreply.github.com>
2021-09-20 11:38:02 -0400

    Update ViewController.swift

--- point clouds/ViewController.swift
+++ point clouds/ViewController.swift
@@ -10,128 +10,224 @@ import Metal
 import MetalKit
 import ARKit
 import CoreBluetooth
+import CoreLocation
 
 extension MTKView : RenderDestinationProvider {
 }
-
-//class ViewController: UIViewController, MTKViewDelegate, ARSessionDelegate, CBCentralManagerDelegate, CBPeripheralDelegate {
-class ViewController: UIViewController, MTKViewDelegate, ARSessionDelegate{
-    
+class ViewController: UIViewController, MTKViewDelegate, ARSessionDelegate, CBPeripheralManagerDelegate{
     var session: ARSession!
     var renderer: Renderer!
-//    var centralManager: CBCentralManager!
-//    var myPeripheral: CBPeripheral!
-        
 
+    var peripheralManager: CBPeripheralManager!
     
+    // static UUID
+    var serviceUUID: CBUUID = CBUUID(string: "0846BFB3-6A8C-48AC-88F5-06D3E8681068")
+    var characteristicUUID: CBUUID = CBUUID(string: "0846BFB3-6A8C-48AC-88F5-06D3E8681068")
+    let advertisementDataLocalNameKey : String = "LiDAR Phone"
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        
+
         // Set the view's delegate
         session = ARSession()
         session.delegate = self
-        
+
         // Set the view to use the default device
         if let view = self.view as? MTKView {
             view.device = MTLCreateSystemDefaultDevice()
             view.backgroundColor = UIColor.clear
             view.delegate = self
-            
+
             guard view.device != nil else {
                 print("Metal is not supported on this device")
                 return
             }
-            
+
             // Configure the renderer to draw to the view
             renderer = Renderer(session: session, metalDevice: view.device!, renderDestination: view)
-            
+
             renderer.drawRectResized(size: view.bounds.size)
         }
-        
-        
+
         // Do any additional setup after loading the view.
-        //centralManager = CBCentralManager(delegate: self, queue: nil)
-        MyPeripheralManager.shared.start()
+        
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
+
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
 
         // Run the view's session
         session.run(configuration)
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
+
         // Pause the view's session
         session.pause()
     }
-    
+
     // MARK: - MTKViewDelegate
-    
+
     // Called whenever view changes orientation or layout is changed
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
         renderer.drawRectResized(size: size)
     }
-    
+
     // Called whenever the view needs to render
     func draw(in view: MTKView) {
         renderer.update()
     }
-    
-    
-    //MARK: - BlueTooth
-//    func centralManagerDidUpdateState(_ central: CBCentralManager) {
-//        if central.state == CBManagerState.poweredOn {
-//            print("BLE on")
-//            central.scanForPeripherals(withServices: nil, options: nil)
-//        }
-//        else {
-//            print("Something wrongw BLE")
-//        }
-//    }
-//
-//    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-//        //print(peripheral.name ?? "default value")
-//        if let pname = peripheral.name {
-//            if pname == "nick-pi" {
-//                print(peripheral.name!)
-//                self.centralManager.stopScan()
-//
-//                self.myPeripheral = peripheral
-//                self.myPeripheral.delegate = self
-//                self.centralManager.connect(peripheral, options: nil)
-//                print(peripheral)
-//            }
-//        }
-//    }
-//
-//    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-//        self.myPeripheral.discoverServices(nil)
-//    }
+
+    //MARK: - Peripheral BlueTooth
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        if peripheral.state == .poweredOn {
+            print("peripheral on")
+            let mutableservice : CBMutableService = CBMutableService(type: serviceUUID, primary: true)
+            let mutableCharacteristic : CBMutableCharacteristic = CBMutableCharacteristic(type: characteristicUUID, properties: [.write, .read], value: nil, permissions: [CBAttributePermissions.writeable, CBAttributePermissions.readable])
+            mutableservice.characteristics = [mutableCharacteristic]
+            peripheral.add(mutableservice)
+            print(mutableservice)
+            peripheral.startAdvertising([CBAdvertisementDataLocalNameKey: advertisementDataLocalNameKey])
+        }
+    }
+
+    func peripheralManager(_ peripheral: CBPeripheralManager, didAdd service: CBService, error: Error?) {
+         if let error = error {
+            print("Add service failed: \(error.localizedDescription)")
+            return
+        }
+        print("Add service succeeded")
+
+    func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: Error?) {
+        if let error = error {
+            print("Start advertising failed: \(error.localizedDescription)")
+            return
+        }
+        print("Start advertising succeeded")
+    }
+
+
+    }
 }
-    
-  
 
-    // MARK: - ARSessionDelegate
-    
-//    func session(_ session: ARSession, didFailWithError error: Error) {
-//        // Present an error message to the user
-//
-//    }
-//
-//    func sessionWasInterrupted(_ session: ARSession) {
-//        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-//
-//    }
-//
-//    func sessionInterruptionEnded(_ session: ARSession) {
-//        // Reset tracking and/or remove existing anchors if consistent tracking is required
-//
-//    }
 
+
+    
+
+
+
+
+
+
+
+
+
+// MARK: - v1
+//class ViewController: UIViewController, MTKViewDelegate, ARSessionDelegate, CBPeripheralManagerDelegate{
+//    var session: ARSession!
+//    var renderer: Renderer!
+//
+//    var peripheralManager: CBPeripheralManager!
+//    var serviceUUID: CBUUID = CBUUID()
+//    var characteristicUUID: CBUUID = CBUUID()
+//    let advertisementDataLocalNameKey : String = "LiDAR Phone"
+//
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Set the view's delegate
+//        session = ARSession()
+//        session.delegate = self
+//
+//        // Set the view to use the default device
+//        if let view = self.view as? MTKView {
+//            view.device = MTLCreateSystemDefaultDevice()
+//            view.backgroundColor = UIColor.clear
+//            view.delegate = self
+//
+//            guard view.device != nil else {
+//                print("Metal is not supported on this device")
+//                return
+//            }
+//
+//            // Configure the renderer to draw to the view
+//            renderer = Renderer(session: session, metalDevice: view.device!, renderDestination: view)
+//
+//            renderer.drawRectResized(size: view.bounds.size)
+//        }
+//
+//        // Do any additional setup after loading the view.
+//        let deviceUUID: String = UIDevice.current.identifierForVendor!.uuidString
+//        serviceUUID = CBUUID(string: deviceUUID)
+//        characteristicUUID = CBUUID(string: deviceUUID)
+//        peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
+//
+//    }
+//
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//
+//        // Create a session configuration
+//        let configuration = ARWorldTrackingConfiguration()
+//
+//        // Run the view's session
+//        session.run(configuration)
+//    }
+//
+//    override func viewWillDisappear(_ animated: Bool) {
+//        super.viewWillDisappear(animated)
+//
+//        // Pause the view's session
+//        session.pause()
+//    }
+//
+//    // MARK: - MTKViewDelegate
+//
+//    // Called whenever view changes orientation or layout is changed
+//    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+//        renderer.drawRectResized(size: size)
+//    }
+//
+//    // Called whenever the view needs to render
+//    func draw(in view: MTKView) {
+//        renderer.update()
+//    }
+//
+//    //MARK: - Peripheral BlueTooth
+//    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+//        if peripheral.state == .poweredOn {
+//            print("peripheral on")
+//            let mutableservice : CBMutableService = CBMutableService(type: serviceUUID, primary: true)
+//            let mutableCharacteristic : CBMutableCharacteristic = CBMutableCharacteristic(type: characteristicUUID, properties: [.write, .read], value: nil, permissions: [CBAttributePermissions.writeable, CBAttributePermissions.readable])
+//            mutableservice.characteristics = [mutableCharacteristic]
+//            peripheral.add(mutableservice)
+//            peripheral.startAdvertising([CBAdvertisementDataLocalNameKey: advertisementDataLocalNameKey])
+//        }
+//    }
+//
+//    func peripheralManager(_ peripheral: CBPeripheralManager, didAdd service: CBService, error: Error?) {
+//         if let error = error {
+//            print("Add service failed: \(error.localizedDescription)")
+//            return
+//        }
+//        print("Add service succeeded")
+//
+//    func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: Error?) {
+//        if let error = error {
+//            print("Start advertising failed: \(error.localizedDescription)")
+//            return
+//        }
+//        print("Start advertising succeeded")
+//    }
+//
+//
+//    }
+//}
