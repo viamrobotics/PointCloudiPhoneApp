4831169defa3b7abcd4daf8d199c72abf2e86a94
Nick Franczak <nicholasgfranczak@gmail.com>
2021-10-17 22:41:12 -0400

    fix UIWindows

--- PointClouds/point clouds/AppDelegate.swift
+++ PointClouds/point clouds/AppDelegate.swift
@@ -10,13 +10,27 @@ import UIKit
 //@main
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    var server: Server!
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
+    }
+    
+    // MARK: UISceneSession Lifecycle
+
+    func application(_: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options _: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_: UIApplication, didDiscardSceneSessions _: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -34,6 +48,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        
     }
 
 
